f5c5e20f7afed17e3bdd4a5c2f4c62f7872a2bf2
Robin Stefani <robin.stefani@netwrix.com>
2025-10-28 16:41:42 -0400

    Added new 1Secure course on Alerts and Risks.

--- src/training/1secure/index.js
+++ src/training/1secure/index.js
@@ -5,4 +5,5 @@ export { default as N1SIntroMO } from './3600-2.md';
 export { default as N1SIntroConf } from './3600-3.md';
 export { default as N1SIntroData } from './3600-4.md';
 export { default as N1SIntroReport } from './3600-5.md';
+export { default as N1SIntroAlertRisk } from './3600-6.md';
 export { default as N1SAdditional } from './additional.md';
